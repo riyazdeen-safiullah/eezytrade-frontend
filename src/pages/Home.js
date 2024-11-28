7e08f591f5b6d8a8edb17bc51dece450969b204d
2B-FOUND <official@2b-found.com>
2024-11-29 03:51:58 +0530

    Home Screen Fixes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,30 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
+  const handleHomeLogin = () => {
+    navigate("/login");
+  };
+  const handleHomeProfile = () => {
+    navigate("/profile");
+  };
+  const handleHomeConfig = () => {
+    navigate("/config");
+  };
   return (
     <div>
       <h1>Welcome to EezyTrade!</h1>
-      <p>Your trading automation platform.</p>
+      <h2>Your trading automation platform.</h2>
+      <button onClick={handleHomeLogin} className="btn-signup-link" type="button">
+        Login
+      </button>
+      <button onClick={handleHomeProfile} className="btn-signup-link" type="button">
+        Profile
+      </button>
+      <button onClick={handleHomeConfig} className="btn-signup-link" type="button">
+        Config
+      </button>
     </div>
   );
 };
